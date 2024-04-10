b1c37ff6adf1db828e0f7ee1fab1d132a830ec41
nando8660 <75550624+nando8660@users.noreply.github.com>
2024-04-10 19:41:44 -0300

    Update c632467001.lua

--- script/c632467001.lua
+++ script/c632467001.lua
@@ -37,7 +37,7 @@ function s.initial_effect(c)
 end
 --SS
 function s.spcon(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 ands Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
