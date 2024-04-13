803db6b6ea6020a2de8a4d372496d34a1dcdb0e2
nando8660 <75550624+nando8660@users.noreply.github.com>
2024-04-13 19:51:20 -0300

    Update c123012789.lua

--- script/c123012789.lua
+++ script/c123012789.lua
@@ -26,6 +26,24 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
     e2:SetOperation(s.addcounter)
     Duel.RegisterEffect(e2,tp)
 	Debug.Message("Registro de LP: "..Ether)
+	--lose
+	local e4=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.losecon)
+	e3:SetOperation(s.loseop)
+    Duel.RegisterEffect(e3,tp)
+	--win
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetCountLimit(1)
+	e4:SetCondition(s.wincon)
+	e4:SetOperation(s.winop)
+    Duel.RegisterEffect(e4,tp)
 end
 function s.countercon(e, tp, eg, ep, ev, re, r, rp)
     return ep==tp
@@ -35,3 +53,16 @@ function s.addcounter(e,tp,eg,ep,ev,re,r,rp)
 	Ether=ev+Ether
 	Debug.Message("Registro de LP: "..Ether)
 end
+function s.wincon(e,tp,eg,ep,ev,re,r,rp)
+	return Ether>=10000
+end
+function s.winop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Win(tp,WIN_REASON_DECK_MASTER)
+end
+function s.losecon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetLP(tp)>=10499
+end
+function s.loseop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Win(1-tp,WIN_REASON_DECK_MASTER)
+end
+
