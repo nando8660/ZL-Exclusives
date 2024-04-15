a64d9a04934c227a54aeea897e15dcf3e666e164
nando8660 <75550624+nando8660@users.noreply.github.com>
2024-04-15 16:34:24 -0300

    Update c123012789.lua

--- script/c123012789.lua
+++ script/c123012789.lua
@@ -17,8 +17,9 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	-- Ativa a Skill para o Oponente
-	-- Debug.AddCard(123012789, 1-tp, 1-tp, LOCATION_PUBLIC, 0, 0)	
+	Debug.AddCard(123012789, 1-tp, 1-tp, LOCATION_PUBLIC, 0, 0)	
 	-- Resto dos efeitos
+	Duel.RegisterFlagEffect(tp,107,0,0,0)
 	local c=e:GetHandler()
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
