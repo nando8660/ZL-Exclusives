64faa554af78b544b4f46d58bfd1b156e2705628
nando8660 <75550624+nando8660@users.noreply.github.com>
2024-04-15 18:06:52 -0300

    Update c123012789.lua

--- script/c123012789.lua
+++ script/c123012789.lua
@@ -13,13 +13,40 @@ function s.initial_effect(c)
 	e1:SetLabel(0)
 	e1:SetOperation(s.flipop)
 	c:RegisterEffect(e1)
+	--Testagem
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
 end
+-----------------------
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local tg=eg:GetFirst()
+	return tg:GetSummonType()==SUMMON_TYPE_XYZ
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(rp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,rp,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+-----------------------
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	--Registra Flags para os Jogadores
 	Duel.RegisterFlagEffect(tp,107,0,0,0)
-	-- Cria os efeitos restantes
+	Cria os efeitos restantes
 	local c=e:GetHandler()
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
