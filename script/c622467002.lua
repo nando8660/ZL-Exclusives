2a8cdb39f5b2f2ede9a94e1bfb4e1a507ecc232e
nando8660 <75550624+nando8660@users.noreply.github.com>
2024-04-23 01:04:17 -0300

    Update c622467002.lua

--- script/c622467002.lua
+++ script/c622467002.lua
@@ -22,7 +22,7 @@ function s.initial_effect(c)
 end
 function s.condition(e, tp, eg, ep, ev, re, r, rp)
 	local c=e:GetHandler()
-	return not (Duel.GetTurnPlayer()==tp) and (Duel.GetTurnCount()==1 or (not Duel.GetFlagEffect(tp, 109)==0) or c:IsFaceDown())
+	return not (Duel.GetTurnPlayer()==tp) and (Duel.GetTurnCount()==1 or (not Duel.GetFlagEffect(tp, 109)==0) or c:IsPosition(POS_FACEDOWN))
 		and Duel.GetCustomActivityCount(id,1-tp,ACTIVITY_CHAIN)>4
 end
 function s.filter(c, e, tp)
