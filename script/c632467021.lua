f29a2111fb64a5f34b258da0625f6360ea370808
nando8660 <75550624+nando8660@users.noreply.github.com>
2024-05-14 00:21:10 -0300

    Update c632467021.lua

--- script/c632467021.lua
+++ script/c632467021.lua
@@ -32,7 +32,8 @@ function s.desfilter(c, e)
     if e:GetHandler():IsLocation(LOCATION_EMZONE) then
         return c:GetSequence()<5 and c:GetSequence()==e:GetHandler():GetSequence() and c:IsLocation(LOCATION_MZONE)
     elseif e:GetHandler():IsLocation(LOCATION_MZONE) and e:GetHandler():GetSequence()<5 then
-        return c:GetSequence()==e:GetHandler():GetSequence() and (c:GetSequence()>=5 or (c:GetSequence()<5 and c:IsControler(1-tp)))
+        return c:GetSequence()==e:GetHandler():GetSequence() and (c:GetSequence()>=5 or (c:GetSequence()<5 
+		and c:GetControler()==e:GetHandler():GetController()))
     elseif e:GetHandler():IsLocation(LOCATION_SZONE) then
         return c:GetSequence()<5 and c:GetSequence()==e:GetHandler():GetSequence() and c:IsLocation(LOCATION_MZONE)
             and c:GetControler()==e:GetHandler():GetController()
