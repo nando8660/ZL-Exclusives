4cf60370b090915c6e63fdaa6fa97e65c56ecb00
nando8660 <75550624+nando8660@users.noreply.github.com>
2024-05-16 22:45:01 -0300

    Update c632467021.lua

--- script/c632467021.lua
+++ script/c632467021.lua
@@ -2,9 +2,9 @@
 -- Detonitrium Cohete del Incendio
 local s,id=GetID()
 function s.initial_effect(c)
-    --fusion material
-    c:EnableReviveLimit()
-    Fusion.AddProcMixN(c, true, true, 632467022,1,632467023,1)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c, true, true, 632467022,1,632467023,1)
 	--Must be Fusion Summoned by a "Tempor-Ax" card effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -12,18 +12,18 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(s.splimit)
 	c:RegisterEffect(e1)
-    --self destroy on summon
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-    e2:SetCode(EVENT_SUMMON_SUCCESS)
-    e2:SetOperation(s.desop)
-    c:RegisterEffect(e2)
-    local e3=e2:Clone()
-    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-    c:RegisterEffect(e3)
-    local e4=e2:Clone()
-    e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-    c:RegisterEffect(e4)
+	--self destroy on summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	local e4=e2:Clone()
+	e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e4)
 end
 function s.splimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or ((st&SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION and se:GetHandler():IsSetCard(0x21DE))
