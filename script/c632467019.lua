c244c0d5d67852007e562f0c0f91c5d81105b2ff
nando8660 <75550624+nando8660@users.noreply.github.com>
2024-05-16 23:13:37 -0300

    Update c632467019.lua

--- script/c632467019.lua
+++ script/c632467019.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
     --fusion material
     c:EnableReviveLimit()
-    Fusion.AddProcMixN(c, true, true, 632467015,1,632467022,1,632467023,1)
+    Fusion.AddProcMixN(c, false, false, 632467015,1,632467022,1,632467023,1)
 	--Must be Fusion Summoned by a "Tempor-Ax" card effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
