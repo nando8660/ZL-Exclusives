9a897077d1a6908f6ae4e0e15dd6c534b513c1a2
nando8660 <75550624+nando8660@users.noreply.github.com>
2024-05-29 13:16:55 -0300

    Update c632467041.lua

--- script/c632467041.lua
+++ script/c632467041.lua
@@ -3,6 +3,13 @@ local s,id=GetID()
 function s.initial_effect(c)
     --Must be properly summoned before reviving
 	c:EnableReviveLimit()
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.synlimit)
+	c:RegisterEffect(e1)
 	--Special Summon procedure
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
